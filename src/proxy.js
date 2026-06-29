ba1945d1537eb52f0838823f31ad80cfc894535a
SinghRohan333 <rohansingh12345602042@gmail.com>
2026-06-29 17:41:17 +0600

    better auth setup done

---
+++ src/proxy.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { auth } from "./lib/auth";
+
+const authRoutes = ["/login", "/register"];
+
+export async function proxy(request) {
+  const { pathname } = request.nextUrl;
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (authRoutes.includes(pathname)) {
+    if (session) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+    return NextResponse.next();
+  }
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request) { ... }
+
+export const config = {
+  matcher: ["/login", "/register"],
+};
